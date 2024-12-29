e9e5de72292953c52c90643ccf9c517d6472cb4f
adityakiran1423 <adi.kiran@gmail.com>
2024-12-29 12:51:39 +0530

    fixed options and support for err handling

--- ringo/main.rb
+++ ringo/main.rb
@@ -1,15 +1,35 @@
 require "thor"
-# require "Dir"
 
 class Ringo < Thor
     
     desc "list START_POINT", "provides breakdown of all languages used in a directory"
-    # option :d => :int, :desc => "depth to which directory should be traversed"
-    # option :v => :int
+
+    option :depth,
+    :type => :numeric,
+    :desc => "depth to which directory should be traversed",
+    :aliases => "d",
+    :default => 1
+
     def list(start_point=nil)
-        puts "welcome to ringo"
-        puts "entered #{start_point} as the start point for search" if start_point
-        puts "not entered start_point, will use #{Dir.pwd} as start_point"
+        begin
+
+            puts "welcome to ringo"
+            if start_point
+                puts "entered #{start_point} as the start point for search"
+            else
+                path = Dir.pwd
+                path = path[1,path.length-1]
+                2.times {
+                    t = path.index('/')
+                    path = path[t+1,path.length-1]
+                }
+                puts "not entered start_point, will use #{path} as start_point"
+            end
+            puts "depth entered is #{options[:depth]}" if options[:depth]
+            
+        rescue ArguementError
+            puts "Oops, sorry. you seem to have an arguement error"
+        end
     end
 end
 
